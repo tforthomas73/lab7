85e66dca4923ae68672e51c173aa77d2376b9a7c
tforthomas73 <decidueye73@gmail.com>
2019-11-21 18:02:07 -0600

    included more to js

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,12 @@
 function init()
 {
-    
+    document.addEventListener('click', function (event)
+    {
+        if (event.target.matches('#entrybutton'))
+        {
+            var textbox = document.getElementById('entryinput').value;
+            alert('Thomas Carmody: ' + textbox);
+            document.getElementById("textoutput").innerHTML = textbox;
+        }
+    });
 }
